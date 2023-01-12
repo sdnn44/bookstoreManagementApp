0cd9882b206cb43659261e1ea4ecfd26fb0da3b0
sdnn44 <slidennnq@gmail.com>
2023-01-12 03:40:09 +0100

    Added main page template displaying certain data from database

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -4,7 +4,7 @@ package com.example.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.bookstore"})
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
