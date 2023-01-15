3f2138940bc94fa2a0ad386581a75dae9964c6c7
sdnn44 <slidennnq@gmail.com>
2023-01-15 19:45:26 +0100

    Added book details view

--- src/main/java/com/example/bookstore/controller/MainController.java
+++ src/main/java/com/example/bookstore/controller/MainController.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +21,15 @@ public class MainController {
     public String getMainPage(Model model) {
         List<Book> allBooks = bookService.getAllBooks();
         model.addAttribute("books", allBooks);
+
         return "main-page.html";
+    }
+
+    @GetMapping("/book-details")
+    public String getBookDetailsPage(@RequestParam Integer bookId, Model model) {
+        Book specificBook = bookService.getBookById(bookId).orElse(null);
+        model.addAttribute("book", specificBook);
+
+        return "book-details-page.html";
     }
 }
